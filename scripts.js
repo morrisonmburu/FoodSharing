8bed98e511f67c6c124c39d374b9b661e87f2d38
MorrisMburu <morrisonmburu7@gmail.com>
2018-05-16 08:15:09 +0300

    Add animate method, all functions and variables

--- scripts.js
+++ scripts.js
@@ -3,4 +3,28 @@
 
 //user Interface logic
 $(document).ready(function(){
+	var animationSpeed = 2000;
+	var total = 1;
+	var interval;
+function startSlider(){
+	interval = setInterval(function(){
+		$("#img0 #img2").animate({"margin-left": "-=1300"}, animationSpeed, function(){
+			total++;
+			if (total === $("#img0 #img2 #img21").length) {
+			total = 1;
+			$("#img0 #img2").css('margin-left',0);
+		}
+			});
+
+		
+	}, 3000);
+}
+
+function stopslider(){
+	clearInterval(interval);
+}
+$("#img0").on('mouseenter', stopslider).on('mouseleave',startSlider);
+
+startSlider();	
+})
 
